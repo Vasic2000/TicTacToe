e31d7c3fb67416d0992f15d3d010cb041e8d7015
vasic2000 <vasic2000@yandex.ru>
2021-12-30 11:06:05 +0300

    Первый и второй экраны

--- app/src/main/java/ru/vasic2000/tictactoe/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/tictactoe/MainActivity.kt
@@ -2,19 +2,36 @@ package ru.vasic2000.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlin.system.exitProcess
 
 private var backPressedTime: Long = 0
 private lateinit var backToast: Toast
 
+private lateinit var backgroundImage : ImageView
+private lateinit var easyLevelBtn : Button
+private lateinit var hardLevelBtn : Button
+private lateinit var impossibleLevelBtn : Button
+private lateinit var startGameBtn : Button
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         getSupportActionBar()?.hide()
+
+        MakeStartScreen()
+    }
+
+    private fun MakeStartScreen() {
+        backgroundImage = findViewById(R.id.main_background)
+        easyLevelBtn = findViewById(R.id.easyLevelBtn)
+        hardLevelBtn = findViewById(R.id.hardLevelBtn)
+        impossibleLevelBtn = findViewById(R.id.impossiblrLevelBtn)
+        startGameBtn = findViewById(R.id.buttonStart)
         BtnStart()
     }
 
@@ -39,6 +56,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun GameStart() {
+        backgroundImage.setImageResource(R.drawable.background)
 
+        startGameBtn.visibility = View.INVISIBLE
+
+        easyLevelBtn.visibility = View.VISIBLE
+        hardLevelBtn.visibility = View.VISIBLE
+        impossibleLevelBtn.visibility = View.VISIBLE
     }
 }
