08e1de13d242ec2d800246c2a2df93f4f0536777
vasic2000 <vasic2000@yandex.ru>
2021-12-30 19:21:48 +0300

    Застрял с прорисовкой линий с задержкой

---
+++ app/src/main/java/ru/vasic2000/tictactoe/GameState.kt
@@ -0,0 +1,8 @@
+package ru.vasic2000.tictactoe
+
+enum class GameState {
+    FIRST_SCREEN,
+    SECOND_SCREEN,
+    GAME_SCREEN,
+    GAME_OVER_SCREEN
+}
--- app/src/main/java/ru/vasic2000/tictactoe/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/tictactoe/MainActivity.kt
@@ -6,17 +6,28 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import java.util.*
 import kotlin.system.exitProcess
 
 private var backPressedTime: Long = 0
 private lateinit var backToast: Toast
+private var gameState: GameState = GameState.FIRST_SCREEN
 
 class MainActivity : AppCompatActivity() {
-    private val backgroundImage : ImageView = findViewById(R.id.main_background)
-    private val easyLevelBtn : Button = findViewById(R.id.easyLevelBtn)
-    private val hardLevelBtn : Button = findViewById(R.id.hardLevelBtn)
-    private val impossibleLevelBtn : Button = findViewById(R.id.impossiblrLevelBtn)
-    private val startGameBtn : Button = findViewById(R.id.buttonStart)
+    private lateinit var backgroundImage: ImageView
+    private lateinit var easyLevelBtn: Button
+    private lateinit var hardLevelBtn: Button
+    private lateinit var impossibleLevelBtn: Button
+    private lateinit var startGameBtn: Button
+
+    private lateinit var leftVerticalLineShadowImage: ImageView
+    private lateinit var rightVerticalLineShadowImage: ImageView
+    private lateinit var topHorizontalLineShadowImage: ImageView
+    private lateinit var bottomHorizontalLineShadowImage: ImageView
+    private lateinit var leftVerticalLineImage: ImageView
+    private lateinit var rightVerticalLineImage: ImageView
+    private lateinit var topHorizontalLineImage: ImageView
+    private lateinit var bottomHorizontalLineImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +37,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun MakeStartScreen() {
+        backgroundImage = findViewById(R.id.main_background)
+        easyLevelBtn = findViewById(R.id.easyLevelBtn)
+        hardLevelBtn = findViewById(R.id.hardLevelBtn)
+        impossibleLevelBtn = findViewById(R.id.impossiblrLevelBtn)
+        startGameBtn = findViewById(R.id.buttonStart)
+
+        leftVerticalLineShadowImage = findViewById(R.id.left_line_shadow)
+        rightVerticalLineShadowImage = findViewById(R.id.right_line_shadow)
+        topHorizontalLineShadowImage = findViewById(R.id.top_line_shadow)
+        bottomHorizontalLineShadowImage = findViewById(R.id.botton_line_shadow)
+        leftVerticalLineImage = findViewById(R.id.left_line)
+        rightVerticalLineImage = findViewById(R.id.right_line)
+        topHorizontalLineImage = findViewById(R.id.top_line)
+        bottomHorizontalLineImage = findViewById(R.id.botton_line)
+
+        backgroundImage.setImageResource(R.drawable.firstscreen)
         BtnStart()
     }
 
@@ -36,19 +63,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        if (backPressedTime + 2000 > System.currentTimeMillis()) {
-            backToast.cancel()
-            super.onBackPressed()
-            exitProcess(0)
-            return
-        } else {
-            backToast = Toast.makeText(this, "Press Back to exit again", Toast.LENGTH_SHORT)
-            backToast.show()
-        }
-        backPressedTime = System.currentTimeMillis()
-    }
-
     private fun GameStart() {
         backgroundImage.setImageResource(R.drawable.background)
 
@@ -57,5 +71,71 @@ class MainActivity : AppCompatActivity() {
         easyLevelBtn.visibility = View.VISIBLE
         hardLevelBtn.visibility = View.VISIBLE
         impossibleLevelBtn.visibility = View.VISIBLE
+
+        BtnEasyGame()
+        BtnHardGame()
+        BtnImpossibleGame()
+    }
+
+    override fun onBackPressed() {
+        when(gameState) {
+            GameState.FIRST_SCREEN -> {
+                if (backPressedTime + 2000 > System.currentTimeMillis()) {
+                    backToast.cancel()
+                    super.onBackPressed()
+                    exitProcess(0)
+                    return
+                } else {
+                    backToast = Toast.makeText(this, "Press Back to exit again", Toast.LENGTH_SHORT)
+                    backToast.show()
+                }
+                backPressedTime = System.currentTimeMillis()
+            }
+            GameState.SECOND_SCREEN -> {}
+            GameState.GAME_SCREEN -> {}
+            GameState.GAME_OVER_SCREEN -> {}
+        }
+    }
+
+    private fun BtnEasyGame() {
+        easyLevelBtn.setOnClickListener {
+            easyLevelBtn.visibility = View.INVISIBLE
+            hardLevelBtn.visibility = View.INVISIBLE
+            impossibleLevelBtn.visibility = View.INVISIBLE
+            DrawLevel()
+        }
+    }
+
+    private fun BtnHardGame() {
+        hardLevelBtn.setOnClickListener {
+            easyLevelBtn.visibility = View.INVISIBLE
+            hardLevelBtn.visibility = View.INVISIBLE
+            impossibleLevelBtn.visibility = View.INVISIBLE
+            DrawLevel()
+        }
+    }
+
+    private fun BtnImpossibleGame() {
+        impossibleLevelBtn.setOnClickListener {
+            easyLevelBtn.visibility = View.INVISIBLE
+            hardLevelBtn.visibility = View.INVISIBLE
+            impossibleLevelBtn.visibility = View.INVISIBLE
+            DrawLevel()
+        }
+    }
+
+    private fun DrawLevel() {
+        backgroundImage.setImageResource(R.drawable.background)
+        leftVerticalLineShadowImage.visibility = View.VISIBLE
+        leftVerticalLineImage.visibility = View.VISIBLE
+
+        rightVerticalLineShadowImage.visibility = View.VISIBLE
+        rightVerticalLineImage.visibility = View.VISIBLE
+
+        topHorizontalLineShadowImage.visibility = View.VISIBLE
+        topHorizontalLineImage.visibility = View.VISIBLE
+
+        bottomHorizontalLineShadowImage.visibility = View.VISIBLE
+        bottomHorizontalLineImage.visibility = View.VISIBLE
     }
 }
