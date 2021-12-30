8d594dc1a47fb5357365b44994f033affe29eec6
Vasic2000 <vasic2000@yandex.ru>
2021-12-30 22:23:07 +0300

    Refactoring - притом полный Реализована навигация по кнопке назад

--- app/src/main/java/ru/vasic2000/tictactoe/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/tictactoe/MainActivity.kt
@@ -32,16 +32,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getSupportActionBar()?.hide()
-        MakeStartScreen()
+        InitImages()
+        InitButtons()
+        ShowFirstScreen()
     }
 
-    private fun MakeStartScreen() {
+    private fun InitImages() {
         backgroundImage = findViewById(R.id.main_background)
-        easyLevelBtn = findViewById(R.id.easyLevelBtn)
-        hardLevelBtn = findViewById(R.id.hardLevelBtn)
-        impossibleLevelBtn = findViewById(R.id.impossiblrLevelBtn)
-        startGameBtn = findViewById(R.id.buttonStart)
-
         leftVerticalLineShadowImage = findViewById(R.id.left_line_shadow)
         rightVerticalLineShadowImage = findViewById(R.id.right_line_shadow)
         topHorizontalLineShadowImage = findViewById(R.id.top_line_shadow)
@@ -50,32 +47,107 @@ class MainActivity : AppCompatActivity() {
         rightVerticalLineImage = findViewById(R.id.right_line)
         topHorizontalLineImage = findViewById(R.id.top_line)
         bottomHorizontalLineImage = findViewById(R.id.botton_line)
+    }
 
-        backgroundImage.setImageResource(R.drawable.firstscreen)
+    private fun InitButtons() {
+        startGameBtn = findViewById(R.id.buttonStart)
+        easyLevelBtn = findViewById(R.id.easyLevelBtn)
+        hardLevelBtn = findViewById(R.id.hardLevelBtn)
+        impossibleLevelBtn = findViewById(R.id.impossibleLevelBtn)
+
         BtnStart()
-    }
-
-    private fun BtnStart() {
-        val btnStart = findViewById<Button>(R.id.buttonStart)
-        btnStart.setOnClickListener {
-            GameStart()
-        }
-    }
-
-    private fun GameStart() {
-        backgroundImage.setImageResource(R.drawable.background)
-        easyLevelBtn.visibility = View.VISIBLE
-        hardLevelBtn.visibility = View.VISIBLE
-        impossibleLevelBtn.visibility = View.VISIBLE
-        startGameBtn.visibility = View.INVISIBLE
-
         BtnEasyGame()
         BtnHardGame()
         BtnImpossibleGame()
     }
 
+    private fun ShowFirstScreen() {
+        gameState = GameState.FIRST_SCREEN
+
+        backgroundImage.setImageResource(R.drawable.firstscreen)
+        leftVerticalLineShadowImage.visibility = View.INVISIBLE
+        rightVerticalLineShadowImage.visibility = View.INVISIBLE
+        topHorizontalLineShadowImage.visibility = View.INVISIBLE
+        bottomHorizontalLineShadowImage.visibility = View.INVISIBLE
+        leftVerticalLineImage.visibility = View.INVISIBLE
+        rightVerticalLineImage.visibility = View.INVISIBLE
+        topHorizontalLineImage.visibility = View.INVISIBLE
+        bottomHorizontalLineImage.visibility = View.INVISIBLE
+
+        easyLevelBtn.visibility = View.INVISIBLE
+        hardLevelBtn.visibility = View.INVISIBLE
+        impossibleLevelBtn.visibility = View.INVISIBLE
+
+        startGameBtn.visibility = View.VISIBLE
+    }
+
+    private fun ShowSecondScreen() {
+        gameState = GameState.SECOND_SCREEN
+
+        backgroundImage.setImageResource(R.drawable.background)
+        leftVerticalLineShadowImage.visibility = View.INVISIBLE
+        rightVerticalLineShadowImage.visibility = View.INVISIBLE
+        topHorizontalLineShadowImage.visibility = View.INVISIBLE
+        bottomHorizontalLineShadowImage.visibility = View.INVISIBLE
+        leftVerticalLineImage.visibility = View.INVISIBLE
+        rightVerticalLineImage.visibility = View.INVISIBLE
+        topHorizontalLineImage.visibility = View.INVISIBLE
+        bottomHorizontalLineImage.visibility = View.INVISIBLE
+
+        startGameBtn.visibility = View.INVISIBLE
+
+        easyLevelBtn.visibility = View.VISIBLE
+        hardLevelBtn.visibility = View.VISIBLE
+        impossibleLevelBtn.visibility = View.VISIBLE
+    }
+
+    private fun ShowGameScreen() {
+        gameState = GameState.GAME_SCREEN
+
+        backgroundImage.setImageResource(R.drawable.background)
+        easyLevelBtn.visibility = View.INVISIBLE
+        hardLevelBtn.visibility = View.INVISIBLE
+        impossibleLevelBtn.visibility = View.INVISIBLE
+        startGameBtn.visibility = View.INVISIBLE
+        DrawLevel()
+    }
+
+    //    Методы инициации кнопок
+    private fun BtnStart() {
+        val btnStart = findViewById<Button>(R.id.buttonStart)
+        btnStart.setOnClickListener {
+            ShowSecondScreen()
+        }
+    }
+
+    private fun BtnEasyGame() {
+        easyLevelBtn.setOnClickListener {
+            ShowGameScreen()
+        }
+    }
+
+    private fun BtnHardGame() {
+        hardLevelBtn.setOnClickListener {
+            ShowGameScreen()
+        }
+    }
+
+    private fun BtnImpossibleGame() {
+        impossibleLevelBtn.setOnClickListener {
+            ShowGameScreen()
+        }
+    }
+
+    //    Метод последовательной прорисовки поля, серез ThreadPost
+    private fun DrawLevel() {
+        val levelDraw = LevelDraw(this)
+        val thread = Thread(levelDraw)
+        thread.start()
+    }
+
+    //    Переопределение кнопки назад
     override fun onBackPressed() {
-        when(gameState) {
+        when (gameState) {
             GameState.FIRST_SCREEN -> {
                 if (backPressedTime + 2000 > System.currentTimeMillis()) {
                     backToast.cancel()
@@ -88,42 +160,15 @@ class MainActivity : AppCompatActivity() {
                 }
                 backPressedTime = System.currentTimeMillis()
             }
-            GameState.SECOND_SCREEN -> {}
-            GameState.GAME_SCREEN -> {}
-            GameState.GAME_OVER_SCREEN -> {}
+            GameState.SECOND_SCREEN -> {
+                ShowFirstScreen()
+            }
+            GameState.GAME_SCREEN -> {
+                ShowSecondScreen()
+            }
+            GameState.GAME_OVER_SCREEN -> {
+                ShowSecondScreen()
+            }
         }
-    }
-
-    private fun BtnEasyGame() {
-        easyLevelBtn.setOnClickListener {
-            easyLevelBtn.visibility = View.INVISIBLE
-            hardLevelBtn.visibility = View.INVISIBLE
-            impossibleLevelBtn.visibility = View.INVISIBLE
-            DrawLevel()
-        }
-    }
-
-    private fun BtnHardGame() {
-        hardLevelBtn.setOnClickListener {
-            easyLevelBtn.visibility = View.INVISIBLE
-            hardLevelBtn.visibility = View.INVISIBLE
-            impossibleLevelBtn.visibility = View.INVISIBLE
-            DrawLevel()
-        }
-    }
-
-    private fun BtnImpossibleGame() {
-        impossibleLevelBtn.setOnClickListener {
-            easyLevelBtn.visibility = View.INVISIBLE
-            hardLevelBtn.visibility = View.INVISIBLE
-            impossibleLevelBtn.visibility = View.INVISIBLE
-            DrawLevel()
-        }
-    }
-
-    private fun DrawLevel() {
-        val levelDraw = LevelDraw(this)
-        val thread = Thread(levelDraw)
-        thread.start()
     }
 }
